7b9cf4baf2bb3a6c6f15153c17c7b0a8a58ddef9
hatch <hatch.tech.eng@gmail.com>
2021-08-06 23:54:57 +0900

    UVDrawer追加

---
+++ Editor/EditorImageUVDrawer.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Yorozu.EditorTool.SceneDrawer
+{
+    internal static class EditorImageDrawer
+    {
+        private const string MENU_PATH = SceneDrawerUtility.TOOL_PATH + "Show Image UV";
+
+        [MenuItem(MENU_PATH)]
+        private static void Menu()
+        {
+            EditorPrefs.SetBool(MENU_PATH, !EditorPrefs.GetBool(MENU_PATH, false));
+        }
+
+        [MenuItem(MENU_PATH, true)]
+        private static bool MenuValidate()
+        {
+            UnityEditor.Menu.SetChecked(MENU_PATH, EditorPrefs.GetBool(MENU_PATH, false));
+            return true;
+        }
+
+        private static List<UIVertex> _vertexList = new List<UIVertex>();
+        private static Dictionary<Vector3, List<int>> _dic = new Dictionary<Vector3, List<int>>();
+        private static int _cacheId;
+        private static VertexHelper _cacheHelper;
+        private static class Cache
+        {
+            public static GUIStyle Style;
+            public static FieldInfo FieldInfo;
+            static Cache()
+            {
+                Style = new GUIStyle(GUI.skin.window);
+                Style.padding.bottom -= 20;
+                Style.padding.left -= 7;
+                Style.padding.right -= 7;
+                Style.margin = new RectOffset();
+                Style.richText = true;
+                FieldInfo = typeof(Graphic).GetField("s_VertexHelper", BindingFlags.NonPublic | BindingFlags.Static);
+            }
+        }
+
+        [DrawGizmo(GizmoType.Selected)]
+        private static void DrawGizmo(Image image, GizmoType type)
+        {
+            if (!EditorPrefs.GetBool(MENU_PATH, false))
+                return;
+
+            if (_cacheId != image.GetInstanceID())
+            {
+                _cacheHelper = Cache.FieldInfo.GetValue(image) as VertexHelper;
+                if (_cacheHelper == null)
+                    return;
+                _cacheId = image.GetInstanceID();
+                image.SetVerticesDirty();
+            }
+            _cacheHelper.GetUIVertexStream(_vertexList);
+            _dic.Clear();
+
+            using (new HandlesMatrixScope(image.transform))
+            {
+                for (var index = 0; index < _vertexList.Count; index++)
+                {
+                    var pos = _vertexList[index].position;
+                    if (!_dic.ContainsKey(pos))
+                        _dic.Add(pos, new List<int>());
+                    _dic[pos].Add(index);
+                }
+
+                foreach (var pair in _dic)
+                {
+                    var uv = _vertexList[pair.Value.First()].uv0;
+                    var color = ColorUtility.ToHtmlStringRGB(_vertexList[pair.Value.First()].color);
+                    Handles.Label(pair.Key, $"<color=#{color}>uv:{uv}</color>", Cache.Style);
+                }
+            }
+        }
+    }
+}
