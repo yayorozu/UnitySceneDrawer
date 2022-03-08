b7e97369af8b4ce7a577db09df720de13c3e1453
hatch <hatch.tech.eng@gmail.com>
2022-03-09 00:11:10 +0900

    Graphicを表示して押すことでHierarchy上で選択できるように

--- Editor/EditorGraphicSelectDrawer.cs
+++ Editor/EditorGraphicSelectDrawer.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Yorozu.EditorTool.SceneDrawer
@@ -7,6 +9,17 @@ namespace Yorozu.EditorTool.SceneDrawer
     internal static class EditorGraphicSelectDrawer
     {
 		private const string MENU_PATH = SceneDrawerUtility.TOOL_PATH + "Show Graphic Object";
+		private static HashSet<Graphic> _hashSet;
+
+		static EditorGraphicSelectDrawer()
+		{
+			SceneView.duringSceneGui += SceneGUI;
+			_hashSet = new HashSet<Graphic>();
+			SceneManager.activeSceneChanged += (arg0, scene) =>
+			{
+				_hashSet.Clear();
+			};
+		}
 
 		[MenuItem(MENU_PATH)]
 		private static void Menu()
@@ -21,24 +34,60 @@ namespace Yorozu.EditorTool.SceneDrawer
 			return true;
 		}
 
-		private static class Styles
-		{
-			internal static GUIStyle Label;
-
-			static Styles()
-			{
-				Label = new GUIStyle(GUI.skin.button);
-				Label.padding = new RectOffset(0, 0, 0, 0);
-			}
-		}
-
 		[DrawGizmo(GizmoType.NonSelected | GizmoType.Selected)]
 		private static void DrawGizmo(Graphic graphic, GizmoType type)
 		{
 			if (!EditorPrefs.GetBool(MENU_PATH, false))
 				return;
 
-			Handles.Label(graphic.transform.position, graphic.transform.name, Styles.Label);
+			if (!_hashSet.Contains(graphic))
+			{
+				_hashSet.Add(graphic);
+			}
 		}
+		
+		private static float? ribbonHeight;
+		
+		private static void SceneGUI(SceneView sceneView)
+		{
+			if (!EditorPrefs.GetBool(MENU_PATH, false))
+				return;
+			
+			if (_hashSet.Count <= 0)
+				return;
+			
+			if (!ribbonHeight.HasValue)
+			{
+				var toolbar = new GUIStyle("GV Gizmo DropDown").CalcSize(sceneView.titleContent);
+				ribbonHeight = toolbar.y;
+			}
+			
+			var windowSize = sceneView.position.size;
+			windowSize.y -= ribbonHeight.Value;
+
+			var guiContent = new GUIContent();
+			Handles.BeginGUI();
+			foreach (var graphic in _hashSet)
+			{
+				if (graphic == null)
+				{
+					continue;
+				}
+				guiContent.text = graphic.transform.name;
+				var size = GUI.skin.button.CalcSize(guiContent);
+				
+				var pointInView = sceneView.camera.WorldToViewportPoint(graphic.transform.position);
+				var pointInSceneView = pointInView * windowSize;
+				var screenPoint = pointInSceneView;
+				screenPoint.y = sceneView.position.height - screenPoint.y;
+				var rect = new Rect(screenPoint.x - size.x / 2f, screenPoint.y - size.y / 2f, size.x, size.y);
+				if (GUI.Button(rect, graphic.transform.name))
+				{
+					Selection.activeGameObject = graphic.gameObject;
+				}
+			}
+			Handles.EndGUI();
+		}
+
     }
 }
